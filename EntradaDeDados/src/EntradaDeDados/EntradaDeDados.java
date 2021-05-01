fd662edc30c682bf4507a8f0ed00a743980a52d2
omarlatorre <omarlatorre@gmail.com>
2021-04-30 23:12:22 -0400

    Operadores em Java

---
+++ EntradaDeDados/src/EntradaDeDados/EntradaDeDados.java
@@ -0,0 +1,96 @@
+package EntradaDeDados;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EntradaDeDados {
+
+	public static void main(String[] args) {
+		// Entrada de dados parte 1
+		/*--------------------------------------------------------------------------------*/
+//		/*Ler um texto até a quebra de linha e armazenar em uma variável*/
+//		Scanner sc = new Scanner(System.in);
+//		String name = sc.nextLine();
+//		System.out.println(name);
+//		/*Ler três palavras, uma em cada linha, armazenando cada uma em uma variável*/
+//		String n1 = sc.nextLine();
+//		String n2 = sc.nextLine();
+//		String n3 = sc.nextLine();
+//		System.out.println(n1+" "+n2+" "+n3);
+//		/*
+//		 * Ler três palavras na mesma linha, separadas por espaço, armazenando cada uma
+//		 * em uma variável
+//		 */
+//		String word1 = sc.next();
+//		String word2 = sc.next();
+//		String word3 = sc.next();
+//		System.out.println(word1+" "+word2+" "+word3);
+//		sc.close();
+		/*----------------------------------------------------------------------*/
+		
+		// entrada de dados parte 2
+//		String s = "abcdefgh";
+//
+//		String sub1 = s.substring(3);
+//		String sub2 = s.substring(3, 5);
+//
+//		System.out.println(sub1);
+//		System.out.println(sub2);
+		
+		/*--------------------------------------------------------------------*/
+//		Locale.setDefault(Locale.US);
+//		Scanner sc = new Scanner(System.in);
+//		// 35
+//		int n1 = sc.nextInt();
+//		// Bob Brown
+//		sc.nextLine();
+//		String name = sc.nextLine();
+//		// F
+//		char gender = sc.next().charAt(0);
+//		// b5
+//		String s = sc.next();
+//		char letter = s.charAt(0);
+//		int digit = Integer.parseInt(s.substring(1));
+//		// 4.32
+//		double n2 = sc.nextDouble();
+//		// Maria F 23 1.68
+//		String name2 = sc.next();
+//		char ch = sc.next().charAt(0);
+//		int age = sc.nextInt();
+//		double height = sc.nextDouble();
+//		System.out.println(n1);
+//		System.out.println(name);
+//		System.out.println(gender);
+//		System.out.println(letter);
+//		System.out.println(digit);
+//		System.out.println(n2);
+//		System.out.println(name2);
+//		System.out.println(ch);
+//		System.out.println(age);
+//		System.out.println(height);
+//		sc.close();
+		/*-------------------------------------------------------*/
+		/* Exercicio*/
+//		Locale.setDefault(Locale.US);
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter your full name:");
+//		String fullName = sc.nextLine();
+//		System.out.println("How many bedrooms are there in your house?");
+//		int bedrooms = sc.nextInt();
+//		System.out.println("Enter product price:");
+//		double price = sc.nextDouble();
+//		System.out.println("Enter your last name, age and height (same line):");
+//		String lastName = sc.next();
+//		int age = sc.nextInt();
+//		double height = sc.nextDouble();
+//		System.out.println(fullName);
+//		System.out.println(bedrooms);
+//		System.out.printf("%.2f%n", price);
+//		System.out.println(lastName);
+//		System.out.println(age);
+//		System.out.printf("%.2f%n", height);
+//		sc.close();
+		/*-------------------------------------------------------*/
+	}
+
+}
---
+++ Operadores/src/OperadoresBasicos/OperadoresBasicos.java
@@ -0,0 +1,113 @@
+package OperadoresBasicos;
+
+import java.util.Scanner;
+
+public class OperadoresBasicos {
+
+	public static void main(String[] args) {
+		// Operadores Aritmeticos
+		/*-----------------------------------------------------------*/
+//		int n1 = 3 + 4 * 2;
+//		int n2 = (3 + 4) * 2;
+//		int n3 = 17 % 3;
+//		double n4 = 10.0 / 8.0;
+//		double a = 1.0, b = -3.0, c = -4.0;
+//		double x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+//		System.out.println(n1);
+//		System.out.println(n2);
+//		System.out.println(n3);
+//		System.out.println(n4);
+//		System.out.println(x1);
+		/*-----------------------------------------------------------*/
+		
+		// Operadores de atribuicao
+//		double n1 = 10;
+//		double n2 = 30;
+//		String s = "ABC";
+//		System.out.println(n1);
+//		System.out.println(n2);
+//		System.out.println(s);
+//
+//		System.out.println("-------------");
+//
+//		n1 *= 2;     // n1 = n1 * 2;
+//		n2 += n1;     // n2 = n2 + n1;
+//		s += "DEF";   // s = s + "DEF";
+//		System.out.println(n1);
+//		System.out.println(n2);
+//		System.out.println(s);
+		/*-----------------------------------------------------------*/
+		
+		// Operadores aritmeticos / atribuicao
+//		System.out.println("-------------");
+//		int a = 10;
+//		a++;          // a = a + 1;
+//		System.out.println(" a = "+a);
+//		
+//		int b = 10;
+//		int c = b++;  // c = b; b = b + 1;
+//		System.out.println(" b = "+b);
+//		System.out.println(" c = "+c);
+//
+//		int d = 10;
+//		int e = ++d;  // d = d+1; e = d;
+//		System.out.println(" d = "+d);
+//		System.out.println(" e = "+e);
+		
+		/*-----------------------------------------------------------*/
+		// Operadores de comparacao
+//		int a = 10;
+//		boolean c1 = a < 10; // false
+//		boolean c2 = a < 20; // true
+//		boolean c3 = a > 10; // false
+//		boolean c4 = a > 5;  // true
+//		System.out.println("c1 = "+c1);
+//		System.out.println("c2 = "+c2);
+//		System.out.println("c3 = "+c3);
+//		System.out.println("c4 = "+c4);
+//		System.out.println("------------");
+//		boolean c5 = a <= 10; // true
+//		boolean c6 = a >= 10; // true
+//		boolean c7 = a == 10; // true
+//		boolean c8 = a != 10; // false
+//		System.out.println("c5 = "+c5);
+//		System.out.println("c6 = "+c6);
+//		System.out.println("c7 = "+c7);
+//		System.out.println("c8 = "+c8);
+		
+		/*-----------------------------------------------------------*/
+		// Operadores logicos
+//		boolean c1 = 2 > 3 || 4 != 5; // true   <-   false || true
+//		boolean c2 = !(2>3) && 4 != 5; // true  <-   true && true
+//		System.out.println("c1 = "+c1);
+//		System.out.println("c2 = "+c2);
+//
+//		System.out.println("--------------");
+//
+//		boolean c3 = 10 < 5; // false
+//		boolean c4 = c1 || c2 && c3; // true <- true || (true && false) <=> true || false
+//		System.out.println("c3 = "+c3);
+//		System.out.println("c4 = "+c4);
+		
+		/*-----------------------------------------------------------*/
+		// Operadores bitwise
+//		int n1 = 89;
+//		int n2 = 60;
+//		System.out.println(n1 & n2);
+//		System.out.println(n1 | n2);
+//		System.out.println(n1 ^ n2);
+		
+//		// Leitura de um inteiro e verifica se e maior a 32
+//		Scanner sc = new Scanner(System.in);
+//		int mask = 0b100000;
+//		int n = sc.nextInt();
+//		if ((n & mask) != 0) {
+//			System.out.println("6th bit is true!");
+//		}
+//		else {
+//			System.out.println("6th bit is false");
+//		}
+//		sc.close();
+		/*-----------------------------------------------------------*/
+	}
+}
